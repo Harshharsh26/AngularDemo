334102a2684322b05657879ffa93ab1122d76ff3
harshharsh26 <harshharsh26@gmail.com>
2016-07-01 14:29:48 +0530

    Draft1

---
+++ www/js/app.js
@@ -0,0 +1,44 @@
+﻿//Declare a App
+var app = angular.module('chatApp', ['firebase']);
+//Add Controller for App
+app.controller('chatController', ['$scope', 'Message', function ($scope, Message) {
+
+    $scope.user = "Guest";
+
+    $scope.messages = Message.all;
+
+    $scope.insert = function (message) {
+        Message.create(message);
+    };
+    $scope.delete = function (message) {
+        Message.delete(message);
+    };
+    $scope.update = function (message) {
+        Message.update(message);
+    };
+    $scope.prependItem = function (message) {
+        items.unshift(message);
+    };
+}]);
+app.factory('Message', ['$firebase',
+	function ($firebase) {
+	    var ref = new Firebase('https://fir-project-a8d73.firebaseio.com/');
+	    var messages = $firebase(ref.child('messages')).$asArray();
+
+	    var Message = {
+	        all: messages,
+	        create: function (message) {
+	            return messages.$add(message);
+	        },
+	        get: function (messageId) {
+	            return $firebase(ref.child('messages').child(messageId)).$asObject();
+	        },
+	        delete: function (message) {
+	            return messages.$remove(message);
+	        }
+	    };
+	   
+	    return Message;
+
+	}
+]);
---
+++ www/js/app_auth.js
@@ -0,0 +1,32 @@
+﻿var firebaseApp = angular.module('starter', ['ionic', 'firebase', 'ngCordovaOauth']);
+var fb = new Firebase("https://INSTANCE_ID_HERE.firebaseio.com");
+
+firebaseApp.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+});
+
+
+firebaseApp.controller("ExampleController", function ($scope, $firebaseAuth, $cordovaOauth) {
+
+    var auth = $firebaseAuth(fb);
+
+    $scope.login = function () {
+        $cordovaOauth.facebook("FACEBOOK_APP_ID_HERE", ["email"]).then(function (result) {
+            auth.$authWithOAuthToken("facebook", result.access_token).then(function (authData) {
+                console.log(JSON.stringify(authData));
+            }, function (error) {
+                console.error("ERROR: " + error);
+            });
+        }, function (error) {
+            console.log("ERROR: " + error);
+        });
+    }
+
+});
